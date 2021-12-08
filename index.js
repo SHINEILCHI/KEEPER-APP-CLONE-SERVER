80c63474ea1a9ff512e5ca79648e3d85f87e0e39
SHINEILCHI <neilpatrickelison.npe@gmail.com>
2021-12-08 14:28:49 +0800

    Changed cors origin.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://keeper-app-clone-mern-stack.netlify.app/"
+    origin: "https://keeper-app-clone-mern-stack.netlify.app"
 }));
 
 mongoose.connect(process.env.MONGODB_URI);
