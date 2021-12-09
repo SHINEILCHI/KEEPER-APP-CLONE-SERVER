071bdd0fb3f64f542f1422f33d8519dc11b6fdc2
SHINEILCHI <neilpatrickelison.npe@gmail.com>
2021-12-09 11:58:37 +0800

    Added local origin for testing purposes.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://keeper-app-clone-mern-stack.herokuapp.com"
+    origin: ["https://keeper-app-clone-mern-stack.herokuapp.com", "http://localhost:5500"]
 }));
 
 mongoose.connect(process.env.MONGODB_URI);
